ea01902a31f039d92c5c6e1d547ede58799f2fa8
Peterson Fonseca <petersonsimiao1@gmail.com>
2019-08-25 03:22:29 -0300

    created component container something

---
+++ src/components/atoms/a-pluss-icon.vue
@@ -0,0 +1,27 @@
+<template>  
+    <span class="plus-icon"></span>
+</template>
+
+<style scoped>
+.plus-icon:before{
+	content: '';
+    width: 20px;
+    height: 5px;
+    background-color: #000;
+    position: absolute;
+    border-radius: 10px;
+    top: 22px;
+    left: 15px;
+    transform: rotate(90deg);
+}
+.plus-icon:after{
+	content: '';
+    width: 20px;
+    height: 5px;
+    background-color: #000;
+    position: absolute;
+    border-radius: 10px;
+    top: 22px;
+    left: 15px;
+}
+</style>
---
+++ src/components/molecules/m-container-something.vue
@@ -0,0 +1,34 @@
+<template>
+	<div class="add-something">
+		<div class="btn">
+			<span class="plus-icon"></span>
+		</div>
+	</div>  
+</template>
+
+<style scoped>
+.btn{
+    width: 50px;
+    height: 50px;
+    background-image: radial-gradient( #fbcc9b, #ff8404 );
+    background-color: #606060;
+    border-radius: 50%;
+    position: relative;
+    cursor: pointer;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+.add-something{
+    width: 130px;
+    height: 130px;
+    background-image: radial-gradient( rgba( 96,96,96, 0 ) , rgba( 96,96,96, .1 ) );
+    box-shadow: 0 0px 5px #606060;
+    border-radius: 20px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+</style>
