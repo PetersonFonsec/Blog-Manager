2aab41295e401eaf21f38887161867b1fcd02d66
Peterson Fonseca <petersonsimiao1@gmail.com>
2019-10-15 10:22:44 -0300

    add module apidoc and created doc for this api

---
+++ BackEnd/public/apidoc/api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "pet_blog_namager",
+  "version": "1.0.0",
+  "description": "",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-10-15T13:20:36.607Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
--- BackEnd/src/routes/user.js
+++ BackEnd/src/routes/user.js
@@ -11,32 +11,183 @@ module.exports = app => {
 
     const { findOne, createAdmin, findUserLogged, updateLogged } = User
 
-
-    /**
-     * @api {get} / API Status
-     * @apiGroup Status
-     * @apiSuccess {String} Status Managem de status da API
-     * @apiSuccessExample {json} Sucesso
-     *      HTTP/1.1 200 OK
-     *      {"status":"NTASK"}
-     */
-
     app.route('/user')
+    /**
+     * @api {get} /user Read data for user
+     * @apiGroup User
+     * @apiSuccess { Object } object containing an array of users
+     * @apiSuccessExample { Json } Sucesso
+     *      HTTP/1.1 200 OK
+     *      {
+            "result": [
+                {
+                    "avatar": "avatar-default",
+                    "_id": "5da496053e2c520d2e5c4109",
+                    "name": "peterson",
+                    "email": "peterson@tests.com"
+                },
+                {
+                    "avatar": "avatar-default",
+                    "_id": "5da496503e2c520d2e5c410a",
+                    "name": "peterson",
+                    "email": "petersonfonsca@tests.com"
+                }
+            ]
+        }
+    */
         .get( find )
+    /**
+     * @api {post} /user Create a new user
+     * @apiGroup User
+     * @apiParam { String } email email for user
+     * @apiParam { String } nome nome for user
+     * @apiParam { String } password password for user
+     * @apiParamExample { Json } Input 
+     *  {
+            "avatar": "avatar-default",
+            "_id": "5da496053e2c520d2e5c4109",
+            "name": "peterson",
+            "email": "peterson@tests.com"
+        }
+     * @apiSuccess { Object } Object with fields avatar, name, email and _id
+     * @apiSuccessExample { Json } Sucesso
+     *      HTTP/1.1 200 OK
+     *      {
+                "result": {
+                    "avatar": "avatar-default",
+                    "_id": "5da5bbd2eb028026b6efeaed",
+                    "name": "peterson",
+                    "email": "Peterson@test.com",
+                    "password": "$2a$10$fPvik9bqaK8PO7/42H/f2eDzWTcPAxqiAkLIZ.5FE396tPUUrTFJi",
+                    "admin": false,
+                    "__v": 0
+                },
+                "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZGE1YmJkMmViMDI4MDI2YjZlZmVhZWQiLCJhZG1pbiI6ZmFsc2UsImlhdCI6MTU3MTE0MjYxMCwiZXhwIjoxNTcxMTQ4NjEwfQ.zhvS6bcxd-ov-B5Ky_7YkfYlKCfA2xIIvbe_hveJYiw"
+            }
+     */
         .post( create )
 
     app.route('/userLogged')
         .all( validToken )
+        /**
+         * @api {get} /userLogged Read data for user logged
+         * @apiGroup UserLogged
+         * @apiHeader { String } Authorization with Token for user
+         * @apiHeaderExample { Json } Authorization
+         * { 
+         *  "Authorization" : "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZGE1YmJkMmViMDI4MDI2YjZlZmVhZWQiLCJhZG1pbiI6ZmFsc2UsImlhdCI6MTU3MTE0MjYxMCwiZXhwIjoxNTcxMTQ4NjEwfQ.zhvS6bcxd-ov-B5Ky_7YkfYlKCfA2xIIvbe_hveJYiw" 
+         * }
+         * @apiSuccess { String } Object with fields avatar, name, email and _id         
+         * @apiSuccessExample { Json } Sucesso
+         *      HTTP/1.1 200 OK
+         *       {
+                    "result": 
+                        {
+                            "avatar": "avatar-default",
+                            "_id": "5da496053e2c520d2e5c4109",
+                            "name": "peterson",
+                            "email": "peterson@tests.com"
+                        }
+                }
+        */
         .get( findUserLogged )
+        /**
+         * @api {put} /userLogged  update data for user logged
+         * @apiGroup UserLogged
+         * @apiSuccess { Object } Object with fields avatar, name, email and _id
+         * @apiParam { String } avatar
+         * @apiParam { String } name
+         * @apiParam { String } email
+         * @apiParamExample { Json } 
+         * {
+         *   "name" : "peterson fonseca"
+         * }
+         * @apiHeader { String } Authorization with Token for user
+         * @apiHeaderExample { Json } Authorization
+         * { 
+         *  "Authorization" : "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZGE1YmJkMmViMDI4MDI2YjZlZmVhZWQiLCJhZG1pbiI6ZmFsc2UsImlhdCI6MTU3MTE0MjYxMCwiZXhwIjoxNTcxMTQ4NjEwfQ.zhvS6bcxd-ov-B5Ky_7YkfYlKCfA2xIIvbe_hveJYiw" 
+         * }
+         * @apiSuccessExample { Json } Sucesso
+         *      HTTP/1.1 200 OK
+         *      {
+                    "result": {
+                        "avatar": "avatar-default",
+                        "_id": "5da5bbd2eb028026b6efeaed",
+                        "name": "peterson fonseca",
+                        "email": "Peterson@test.com",
+                        "admin": false,
+                        "__v": 0
+                    }
+                }
+        */
         .put( updateLogged )
+        /**
+         * @api {post} /userLogged valid password for user logged
+         * @apiGroup UserLogged
+         * @apiParam { String } password
+         * @apiParamExample 
+         *  { 
+         *      password: "123456"
+         *  }
+         * @apiHeader { String } Authorization with Token for user
+         * @apiHeaderExample { Json } Authorization
+         * { 
+         *  "Authorization" : "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZGE1YmJkMmViMDI4MDI2YjZlZmVhZWQiLCJhZG1pbiI6ZmFsc2UsImlhdCI6MTU3MTE0MjYxMCwiZXhwIjoxNTcxMTQ4NjEwfQ.zhvS6bcxd-ov-B5Ky_7YkfYlKCfA2xIIvbe_hveJYiw" 
+         * }
+         * @apiSuccess { String } return a String
+         * @apiSuccessExample { Json } Sucesso
+         *      HTTP/1.1 200 OK
+         *      { msg : 'ok' }
+         
+        */
         .post( validPasswordUserLogged )
 
     app.route('/userlogged/changepassword')
         .all( validToken )
+         /**
+         * @api { Post } /userlogged/changepassword
+         * @apiGroup UserLogged
+         * @apiParam { String } password
+         * @apiParamExample 
+         *  { 
+         *      password: "123456789"
+         *  }
+         * @apiHeader { String } Authorization with Token for user
+         * @apiHeaderExample { Json } Authorization
+         * { 
+         *  "Authorization" : "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZGE1YmJkMmViMDI4MDI2YjZlZmVhZWQiLCJhZG1pbiI6ZmFsc2UsImlhdCI6MTU3MTE0MjYxMCwiZXhwIjoxNTcxMTQ4NjEwfQ.zhvS6bcxd-ov-B5Ky_7YkfYlKCfA2xIIvbe_hveJYiw" 
+         * }
+         * @apiSuccess { String } return a String
+         * @apiSuccessExample { Json } Sucesso
+         *      HTTP/1.1 200 OK
+         *      {
+                    "msg": "ok"
+                }
+        */
         .put( changePassword )
     
     app.route('/user/admin')
         .all( validToken )
+        /**
+        * @api {post} /user/admin Create new user admin
+        * @apiGroup User Admin
+        * @apiParam { String } password
+        * @apiParamExample 
+        *  { 
+        *      password: "123456789"
+        *  }
+        * @apiHeader { String } Authorization with Token for user
+        * @apiHeaderExample { Json } Authorization
+        * { 
+        *  "Authorization" : "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZGE1YmJkMmViMDI4MDI2YjZlZmVhZWQiLCJhZG1pbiI6ZmFsc2UsImlhdCI6MTU3MTE0MjYxMCwiZXhwIjoxNTcxMTQ4NjEwfQ.zhvS6bcxd-ov-B5Ky_7YkfYlKCfA2xIIvbe_hveJYiw" 
+        * }
+        * @apiSuccess { String } return a String
+        * @apiSuccessExample { Json } Sucesso
+        *      HTTP/1.1 200 OK
+        *      {
+                   "msg": "ok"
+               }
+       */
         .post( createAdmin )
 
     app.route('/user/:id')
